a622f5a352545c415d2720d16a1f1d522d818b00
o1was <o1was@gmail.com>
2025-08-04 16:14:46 +0800

    ✨ Feat(可选参数): 可选参数
    可选参数

--- proto-gen-go/fund/v1/fund.pb.go
+++ proto-gen-go/fund/v1/fund.pb.go
@@ -306,6 +306,7 @@ type TransferInReq struct {
 	DepositAmount   string                 `protobuf:"bytes,3,opt,name=deposit_amount,json=depositAmount,proto3" json:"deposit_amount,omitempty"`         // 存款金额
 	MerchantOrderId string                 `protobuf:"bytes,4,opt,name=merchant_order_id,json=merchantOrderId,proto3" json:"merchant_order_id,omitempty"` // 商户订单ID
 	ExtraParams     map[string]string      `protobuf:"bytes,5,rep,name=extra_params,json=extraParams,proto3" json:"extra_params,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"bytes,2,opt,name=value"`
+	GameId          *int64                 `protobuf:"varint,6,opt,name=game_id,json=gameId,proto3,oneof" json:"game_id,omitempty"` // 游戏ID 可选
 	unknownFields   protoimpl.UnknownFields
 	sizeCache       protoimpl.SizeCache
 }
@@ -373,6 +374,13 @@ func (x *TransferInReq) GetExtraParams() map[string]string {
 		return x.ExtraParams
 	}
 	return nil
+}
+
+func (x *TransferInReq) GetGameId() int64 {
+	if x != nil && x.GameId != nil {
+		return *x.GameId
+	}
+	return 0
 }
 
 // 发起转入操作响应
@@ -513,6 +521,7 @@ type TransferOutReq struct {
 	MerchantOrderId  string                 `protobuf:"bytes,3,opt,name=merchant_order_id,json=merchantOrderId,proto3" json:"merchant_order_id,omitempty"` // 商户订单ID
 	ExtraParams      map[string]string      `protobuf:"bytes,4,rep,name=extra_params,json=extraParams,proto3" json:"extra_params,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"bytes,2,opt,name=value"`
 	WithdrawalAmount string                 `protobuf:"bytes,5,opt,name=withdrawal_amount,json=withdrawalAmount,proto3" json:"withdrawal_amount,omitempty"` // 取款金额->todo 预留字段暂时不会使用[My rules are THE rules.]
+	GameId           *int64                 `protobuf:"varint,6,opt,name=game_id,json=gameId,proto3,oneof" json:"game_id,omitempty"`                        // 游戏ID 可选
 	unknownFields    protoimpl.UnknownFields
 	sizeCache        protoimpl.SizeCache
 }
@@ -580,6 +589,13 @@ func (x *TransferOutReq) GetWithdrawalAmount() string {
 		return x.WithdrawalAmount
 	}
 	return ""
+}
+
+func (x *TransferOutReq) GetGameId() int64 {
+	if x != nil && x.GameId != nil {
+		return *x.GameId
+	}
+	return 0
 }
 
 // 发起转出操作响应
@@ -1937,17 +1953,20 @@ const file_fund_v1_fund_proto_rawDesc = "" +
 	"\bbalances\x18\x01 \x03(\v2*.fund.v1.UserBalanceListData.BalancesEntryR\bbalances\x1a;\n" +
 	"\rBalancesEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\x03R\x03key\x12\x14\n" +
-	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"\xa8\x02\n" +
+	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"\xd2\x02\n" +
 	"\rTransferInReq\x12\x17\n" +
 	"\auser_id\x18\x01 \x01(\x03R\x06userId\x12\x1f\n" +
 	"\vplatform_id\x18\x02 \x01(\x03R\n" +
 	"platformId\x12%\n" +
 	"\x0edeposit_amount\x18\x03 \x01(\tR\rdepositAmount\x12*\n" +
 	"\x11merchant_order_id\x18\x04 \x01(\tR\x0fmerchantOrderId\x12J\n" +
-	"\fextra_params\x18\x05 \x03(\v2'.fund.v1.TransferInReq.ExtraParamsEntryR\vextraParams\x1a>\n" +
+	"\fextra_params\x18\x05 \x03(\v2'.fund.v1.TransferInReq.ExtraParamsEntryR\vextraParams\x12\x1c\n" +
+	"\agame_id\x18\x06 \x01(\x03H\x00R\x06gameId\x88\x01\x01\x1a>\n" +
 	"\x10ExtraParamsEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
-	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"h\n" +
+	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01B\n" +
+	"\n" +
+	"\b_game_id\"h\n" +
 	"\x0eTransferInResp\x12)\n" +
 	"\x04base\x18\x01 \x01(\v2\x15.fund.v1.BaseResponseR\x04base\x12+\n" +
 	"\x04data\x18\x02 \x01(\v2\x17.fund.v1.TransferInDataR\x04data\"\xe2\x01\n" +
@@ -1956,17 +1975,20 @@ const file_fund_v1_fund_proto_rawDesc = "" +
 	"\x11merchant_order_id\x18\x02 \x01(\tR\x0fmerchantOrderId\x121\n" +
 	"\x06status\x18\x03 \x01(\x0e2\x19.common.v1.TransferStatusR\x06status\x12%\n" +
 	"\x0edeposit_amount\x18\x04 \x01(\tR\rdepositAmount\x12#\n" +
-	"\rbalance_after\x18\x05 \x01(\tR\fbalanceAfter\"\xb0\x02\n" +
+	"\rbalance_after\x18\x05 \x01(\tR\fbalanceAfter\"\xda\x02\n" +
 	"\x0eTransferOutReq\x12\x17\n" +
 	"\auser_id\x18\x01 \x01(\x03R\x06userId\x12\x1f\n" +
 	"\vplatform_id\x18\x02 \x01(\x03R\n" +
 	"platformId\x12*\n" +
 	"\x11merchant_order_id\x18\x03 \x01(\tR\x0fmerchantOrderId\x12K\n" +
 	"\fextra_params\x18\x04 \x03(\v2(.fund.v1.TransferOutReq.ExtraParamsEntryR\vextraParams\x12+\n" +
-	"\x11withdrawal_amount\x18\x05 \x01(\tR\x10withdrawalAmount\x1a>\n" +
+	"\x11withdrawal_amount\x18\x05 \x01(\tR\x10withdrawalAmount\x12\x1c\n" +
+	"\agame_id\x18\x06 \x01(\x03H\x00R\x06gameId\x88\x01\x01\x1a>\n" +
 	"\x10ExtraParamsEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
-	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"j\n" +
+	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01B\n" +
+	"\n" +
+	"\b_game_id\"j\n" +
 	"\x0fTransferOutResp\x12)\n" +
 	"\x04base\x18\x01 \x01(\v2\x15.fund.v1.BaseResponseR\x04base\x12,\n" +
 	"\x04data\x18\x02 \x01(\v2\x18.fund.v1.TransferOutDataR\x04data\"\xe9\x01\n" +
@@ -2218,6 +2240,8 @@ func file_fund_v1_fund_proto_init() {
 		return
 	}
 	file_fund_v1_fund_types_proto_init()
+	file_fund_v1_fund_proto_msgTypes[6].OneofWrappers = []any{}
+	file_fund_v1_fund_proto_msgTypes[9].OneofWrappers = []any{}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
